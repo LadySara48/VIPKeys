96de15840151165428288ef198160f2b37d4d07b
Paulina Sara <69171937+LadySara48@users.noreply.github.com>
2023-07-15 19:42:30 +0300

    Clean empty code

--- src/hearlov/vipkeys/command/reedemCommand.php
+++ src/hearlov/vipkeys/command/reedemCommand.php
@@ -12,20 +12,15 @@ use hearlov\vipkeys\forms\ReedemForm;
 
 class reedemCommand extends Command {
 
-	private VIPKeys $owner;
-
     public function __construct(VIPKeys $owner){
         parent::__construct("reedem");
-		$this->owner = $owner;
         $this->setDescription("Reedem Command");
-		$this->setPermission("vipkeys.menu");
+	$this->setPermission("vipkeys.menu");
     }
 
     public function execute(CommandSender $player, string $commandLabel, array $args): bool{
         if($player instanceof Player){
-			$player->sendForm(new ReedemForm());
-			//$event = new keyUseEvent($this->owner, $player, "1372613WQWEQ", "RIQVOPRQW231");
-			//$event->call();
+		$player->sendForm(new ReedemForm());
         }
         return true;
     }
