ff20f4b6fa1a304e4a4d1528e35da5ae1d46b5bd
Paulina Sara <69171937+LadySara48@users.noreply.github.com>
2023-07-15 19:42:54 +0300

    Clean empty code

--- src/hearlov/vipkeys/command/vipkeyCommand.php
+++ src/hearlov/vipkeys/command/vipkeyCommand.php
@@ -12,18 +12,15 @@ use hearlov\vipkeys\forms\VIPKeysMainForm;
 
 class vipkeyCommand extends Command {
 
-	private VIPKeys $owner;
-
     public function __construct(VIPKeys $owner){
         parent::__construct("vipkeys");
-		$this->owner = $owner;
         $this->setDescription("VIP Keys open");
-		$this->setPermission("vipkeys.admin.menu");
+	$this->setPermission("vipkeys.admin.menu");
     }
 
     public function execute(CommandSender $player, string $commandLabel, array $args): bool{
         if($player instanceof Player){
-			$player->sendForm(new VIPKeysMainForm());
+		$player->sendForm(new VIPKeysMainForm());
         }
         return true;
     }
